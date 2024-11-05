c84bdff9f1f8f8198c5d01d07eff1e90919e834f
daovanhung19022000 <101968741+daovanhung19022000@users.noreply.github.com>
2024-11-05 21:06:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ def put_test(value):
         "value" : value
     }
 
+def abc():
+    print("alo")
+
 
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
